bad9018b16634fc3432d6821c67383a8efd3840b
yzzzang <yz1211ch@gmail.com>
2024-11-07 16:56:00 +0900

    first commit

---
+++ climb-on/src/main/java/com/ohgiraffers/climbon/ClimbOnApplication.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.climbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbOnApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ClimbOnApplication.class, args);
+    }
+
+}
