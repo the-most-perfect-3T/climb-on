efb34a3ed89d6cf68d07bdc2250aa162867a52c4
Lee <cookie3013@naver.com>
2024-11-08 18:08:52 +0900

    feature

---
+++ climb-on/src/main/java/com/ohgiraffers/climbon/facilities/controller/FacilitiesServiceController.java
@@ -0,0 +1,62 @@
+/*
+ 작성자: 이승환 - 2024-11-07
+    설명 :
+ */
+
+package com.ohgiraffers.climbon.facilities.controller;
+
+import com.ohgiraffers.climbon.facilities.dto.FacilitiesDTO;
+import com.ohgiraffers.climbon.facilities.service.FacilitiesServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/facilitiesService")
+public class FacilitiesServiceController {
+
+    @Autowired
+    private FacilitiesServiceService facilitiesServiceService; //퍼실리티서비스
+
+    @GetMapping("select")
+    public ModelAndView selectCategory(ModelAndView mv) {
+        // 서비스 리스트 가져오기
+        List<FacilitiesDTO> facilitiesList = facilitiesServiceService.facilitiesList();
+
+        // ModelAndView 생성 (뷰 이름: select, 모델에 시설 목록 추가)
+        mv.setViewName("facilities/facilities") ; // 'select'는 view 이름
+        mv.addObject("facilitiesList", facilitiesList); // 모델에 데이터 추가
+
+
+        return mv; // ModelAndView 반환
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ climb-on/src/main/java/com/ohgiraffers/climbon/facilities/service/FacilitiesServiceService.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.climbon.facilities.service;
+
+public class FacilitiesServiceService {
+}
