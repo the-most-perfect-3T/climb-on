b3190f9fda8222b21b8786aade86314d57026c6a
Lee <cookie3013@naver.com>
2024-11-12 17:37:54 +0900

    feat: 암장조회 검색,카테고리로 조회

--- climb-on/src/main/java/com/ohgiraffers/climbon/facilities/service/FacilitiesService.java
+++ climb-on/src/main/java/com/ohgiraffers/climbon/facilities/service/FacilitiesService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 @Service
 public class FacilitiesService {
 
@@ -18,5 +20,21 @@ public class FacilitiesService {
         List<FacilitiesDTO> facilitiesDTOList = facilitiesDAO.facilitiesList();
 
         return facilitiesDTOList;
+    }
+
+    public List<FacilitiesDTO> categorySelect(int categoryId,String facilityName) {
+
+        List<FacilitiesDTO> facilitiesDTOList = facilitiesDAO.categorySelect(categoryId,facilityName);
+
+        return facilitiesDTOList;
+    }
+
+    public List<FacilitiesDTO> searchOneFacility(String code) {
+        List<FacilitiesDTO> facilities =facilitiesDAO.searchOneFacility(code);
+        return facilities;
+    }
+
+    public List<FacilitiesDTO> getFacilitySuggestions(String code) {
+        return facilitiesDAO.searchOneFacility(code);
     }
 }
