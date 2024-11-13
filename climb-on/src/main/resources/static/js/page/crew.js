b893a740ddd711d7464904e5e10535cf187717fe
NJ-KIM11 <alwaysred1116@gmail.com>
2024-11-13 09:37:27 +0900

    pull 받기 위한 커밋

---
+++ climb-on/src/main/resources/static/js/page/crew.js
@@ -0,0 +1,725 @@
+
+
+/* 커뮤니티/크루-게시글 작성 */
+
+position: relative;
+width: 1920px;
+height: 1636px;
+
+background: #FFFFFF;
+
+
+/* Rectangle 8 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 1920px;
+height: 64px;
+left: 0px;
+top: 0px;
+
+border-bottom: 1px solid #DEE2E6;
+
+
+/* Group 72 */
+
+position: absolute;
+width: 1400px;
+height: 561px;
+left: 260px;
+top: 588px;
+
+
+
+/* 본문 */
+
+position: absolute;
+width: 71px;
+height: 18px;
+left: 260px;
+top: 588px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 25px;
+
+color: #222222;
+
+
+
+/* Rectangle 80 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 1400px;
+height: 520px;
+left: 260px;
+top: 629px;
+
+background: #FFFFFF;
+border: 1px solid #DEE2E6;
+border-radius: 8px;
+
+
+/* 내용을 입력 해주세요.(10,000자 이내) */
+
+position: absolute;
+width: 477px;
+height: 35px;
+left: 281px;
+top: 689px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 22px;
+display: flex;
+align-items: center;
+
+color: #AAAAAA;
+
+
+
+/* image 38 */
+
+position: absolute;
+width: 1051px;
+height: 48px;
+left: 260px;
+top: 626px;
+
+background: url(image.png);
+
+
+/* Group 70 */
+
+position: absolute;
+width: 1400px;
+height: 100px;
+left: 260px;
+top: 308px;
+
+
+
+/* 카테고리 */
+
+position: absolute;
+width: 71px;
+height: 20px;
+left: 261px;
+top: 308px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 25px;
+
+color: #222222;
+
+
+
+/* Rectangle 80 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 1400px;
+height: 60px;
+left: 260px;
+top: 348px;
+
+background: #FFFFFF;
+border: 1px solid #DEE2E6;
+border-radius: 6px;
+
+
+/* 카테고리를 선택해주세요. */
+
+position: absolute;
+width: 362px;
+height: 22px;
+left: 280px;
+top: calc(50% - 22px/2 - 440px);
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 22px;
+/* identical to box height */
+display: flex;
+align-items: center;
+
+color: #AAAAAA;
+
+
+
+/* faAngleDown */
+
+position: absolute;
+width: 25px;
+height: 25px;
+left: 1605px;
+top: 366px;
+
+
+
+/* Vector */
+
+position: absolute;
+width: 18.76px;
+height: 10.94px;
+left: calc(50% - 18.76px/2 - 0.12px);
+top: calc(50% - 10.94px/2 - 0.03px);
+
+background: #6C757D;
+
+
+/* Line 27 */
+
+position: absolute;
+width: 30px;
+height: 0px;
+left: 1575px;
+top: calc(50% - 0px/2 - 455px);
+
+border: 1px solid #6C757D;
+transform: rotate(90deg);
+
+
+/* Group 77 */
+
+position: absolute;
+width: 1400px;
+height: 100px;
+left: 260px;
+top: 448px;
+
+
+
+/* 제목 */
+
+position: absolute;
+width: 71px;
+height: 20px;
+left: 261px;
+top: 448px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 25px;
+
+color: #222222;
+
+
+
+/* Rectangle 80 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 1400px;
+height: 60px;
+left: 260px;
+top: 488px;
+
+background: #FFFFFF;
+border: 1px solid #DEE2E6;
+border-radius: 6px;
+
+
+/* 제목을 입력해주세요. */
+
+position: absolute;
+width: 362px;
+height: 22px;
+left: 280px;
+top: calc(50% - 22px/2 - 300px);
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 22px;
+/* identical to box height */
+display: flex;
+align-items: center;
+
+color: #AAAAAA;
+
+
+
+/* Group 69 */
+
+position: absolute;
+width: 442px;
+height: 72px;
+left: 260px;
+top: 146px;
+
+
+
+/* User 님 크루게시판에서 다양한 크루원들과 교류해 보세요. */
+
+position: absolute;
+width: 442px;
+height: 18px;
+left: 260px;
+top: 200px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 25px;
+
+color: #222222;
+
+
+
+/* 크루게시판 글 쓰기 */
+
+position: absolute;
+width: 406px;
+height: 29px;
+left: 260px;
+top: 146px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 28px;
+line-height: 38px;
+
+color: #222222;
+
+
+
+/* header */
+
+position: absolute;
+width: 1920px;
+height: 64px;
+left: 0px;
+top: 2px;
+
+
+
+/* Rectangle 8 */
+
+box-sizing: border-box;
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+
+border-bottom: 1px solid #DEE2E6;
+
+
+/* menu */
+
+position: absolute;
+left: 30.21%;
+right: 56.51%;
+top: 31.25%;
+bottom: 29.69%;
+
+
+
+/* 시설 */
+
+position: absolute;
+left: 30.21%;
+right: 68.02%;
+top: 31.25%;
+bottom: 29.69%;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 25px;
+/* identical to box height */
+
+color: #000000;
+
+
+
+/* 커뮤니티 */
+
+position: absolute;
+left: 35.1%;
+right: 61.41%;
+top: 31.25%;
+bottom: 29.69%;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 25px;
+/* identical to box height */
+
+color: #000000;
+
+
+
+/* 크루 */
+
+position: absolute;
+left: 41.72%;
+right: 56.51%;
+top: 31.25%;
+bottom: 29.69%;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 25px;
+/* identical to box height */
+
+color: #FF7F27;
+
+
+
+/* CLI'MON */
+
+position: absolute;
+visibility: hidden;
+left: 13.54%;
+right: 78.96%;
+top: 9.38%;
+bottom: 10.94%;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 34px;
+line-height: 51px;
+/* identical to box height */
+
+color: #FF7F27;
+
+
+
+/* CLI'MON */
+
+position: absolute;
+left: 13.54%;
+right: 78.85%;
+top: 17.19%;
+bottom: 14.06%;
+
+font-family: 'Lemon';
+font-style: normal;
+font-weight: 400;
+font-size: 34px;
+line-height: 44px;
+letter-spacing: -0.05em;
+
+color: #FF7F27;
+
+
+
+/* CLI'MON */
+
+position: absolute;
+visibility: hidden;
+left: 13.54%;
+right: 78.85%;
+top: 17.19%;
+bottom: 14.06%;
+
+font-family: 'Lemon';
+font-style: normal;
+font-weight: 400;
+font-size: 34px;
+line-height: 44px;
+letter-spacing: -0.05em;
+
+color: #FF7F27;
+
+
+
+/* faSearch */
+
+position: absolute;
+left: 79.84%;
+right: 18.91%;
+top: 31.25%;
+bottom: 31.25%;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: -0.01%;
+
+background: #6C757D;
+
+
+/* faAdjust */
+
+position: absolute;
+left: 82.55%;
+right: 16.2%;
+top: 31.25%;
+bottom: 31.25%;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+
+background: #6C757D;
+
+
+/* faUser */
+
+position: absolute;
+left: 85.21%;
+right: 13.54%;
+top: 31.25%;
+bottom: 31.25%;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 6.25%;
+right: 6.25%;
+top: 0%;
+bottom: 0%;
+
+background: #6C757D;
+
+
+/* footer 이게진짜 */
+
+position: absolute;
+width: 1920px;
+height: 100px;
+left: 0px;
+top: 1536px;
+
+
+
+/* Rectangle 13 */
+
+box-sizing: border-box;
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+
+background: #FFFFFF;
+border-top: 1px solid #DEE2E6;
+
+
+/* CLI'MON */
+
+position: absolute;
+width: 103px;
+height: 31px;
+left: 260px;
+top: 32px;
+
+font-family: 'Lemon';
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 31px;
+letter-spacing: -0.05em;
+
+color: #6C757D;
+
+
+
+/* 클라이몬 Copyright © 2024 CLI'MON. All Rights Reserved. */
+
+position: absolute;
+width: 270px;
+height: 32px;
+left: 1391px;
+top: 34px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 12px;
+line-height: 16px;
+text-align: right;
+
+color: #6C757D;
+
+
+
+/* Group 74 */
+
+position: absolute;
+width: 80px;
+height: 32px;
+left: calc(50% - 80px/2 + 638px);
+top: calc(50% - 32px/2 + 372px);
+
+
+
+/* faToggleOff */
+
+position: absolute;
+width: 32px;
+height: 32px;
+left: 1558px;
+top: 1174px;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 16.67%;
+bottom: 16.67%;
+
+background: #FF7F27;
+
+
+/* 익명 */
+
+position: absolute;
+width: 37px;
+height: 18px;
+left: 1601px;
+top: 1179px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 22px;
+
+color: #000000;
+
+
+
+/* Group 133 */
+
+position: absolute;
+width: 303px;
+height: 50px;
+left: calc(50% - 303px/2 - 0.5px);
+top: 1306px;
+
+
+
+/* Group 29 */
+
+position: absolute;
+width: 140px;
+height: 50px;
+left: 971px;
+top: 1306px;
+
+
+
+/* Rectangle 2 */
+
+position: absolute;
+width: 140px;
+height: 50px;
+left: 971px;
+top: 1306px;
+
+background: #FF7F27;
+border-radius: 6px;
+
+
+/* 등록 */
+
+position: absolute;
+width: 140px;
+height: 50px;
+left: 971px;
+top: 1306px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 22px;
+display: flex;
+align-items: center;
+text-align: center;
+
+color: #FFFFFF;
+
+
+
+/* Group 76 */
+
+position: absolute;
+width: 140px;
+height: 50px;
+left: 808px;
+top: 1306px;
+
+
+
+/* Rectangle 2 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 140px;
+height: 50px;
+left: 808px;
+top: 1306px;
+
+background: #FFFFFF;
+border: 1px solid #6C757D;
+border-radius: 6px;
+
+
+/* 취소 */
+
+position: absolute;
+width: 140px;
+height: 50px;
+left: 808px;
+top: 1306px;
+
+font-family: 'Noto Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 22px;
+display: flex;
+align-items: center;
+text-align: center;
+
+color: #000000;
+
