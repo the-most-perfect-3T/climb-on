ac592924e814390cd9ace5848a45f2507b213e75
NJ-KIM11 <alwaysred1116@gmail.com>
2024-11-14 20:48:19 +0900

    feat: textEditor API 교체

--- climb-on/src/main/resources/static/js/page/crew.js
+++ climb-on/src/main/resources/static/js/page/crew.js
@@ -1,3 +1,86 @@
+/*const options = {
+    debug: 'info',
+    modules: {
+        toolbar: true,
+    },
+    theme: 'snow'
+}
+
+const quill = new Quill('#editor', options);*/
+
+
+
+
+
+
+
+
+
+
+
+/*import { ClassicEditor,
+    Essentials,
+    Bold,
+    Italic,
+    Underline,
+    Strikethrough,
+    Font,
+    Paragraph,
+    Image,
+    ImageToolbar,
+    SimpleUploadAdapter,
+    ImageUpload } from '/ckeditor5';
+import coreTranslations from '/ckeditor5/translations/pl.js';
+import '/ckeditor5/ckeditor5.css';
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        translations: coreTranslations,
+        plugins: [ Essentials, Bold, Italic, Underline, Strikethrough, Font, Paragraph,
+            Image, ImageToolbar, SimpleUploadAdapter,
+            ImageUpload
+        ],
+        image:{
+            insert:{
+                integrations: ['upload'],
+                // type:'auto'
+            }
+        },
+        toolbar: [
+            'undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', '|',
+            'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
+            'insertImage'
+        ],
+        simpleUpload: {
+            // The URL that the images are uploaded to.
+            uploadUrl: '/crew/imgUpload',
+
+            // Enable the XMLHttpRequest.withCredentials property.
+            withCredentials: true,
+
+            // Headers sent along with the XMLHttpRequest to the upload server.
+            headers: {
+                'X-CSRF-TOKEN': 'CSRF-Token',
+                Authorization: 'Bearer <JSON Web Token>'
+            }
+        },
+        language: {
+            ui: 'ko',
+            content: 'ko'
+        }
+    })
+    .then( editor => {
+        window.editor = editor;
+    } )
+    .catch( err => {
+        console.error( err.stack );
+    } );
+
+
+
+
+
+
 const onSubmitHandler2 = event => {
     event.preventDefault();
     let result  = event.target;
@@ -15,7 +98,7 @@ const onSubmitHandler2 = event => {
     let content = document.getElementById("editor");
 
 
-    /*let textWithoutHtml = content.innerHTML
+    /!*let textWithoutHtml = content.innerHTML
         .replace(/<\/?[^>]+(>|$)/g, "") // Remove HTML tags
         .replace(/&nbsp;/g, " ") // Replace &nbsp; with a space
 
@@ -23,7 +106,7 @@ const onSubmitHandler2 = event => {
     let cleanText = textWithoutHtml.replace(/\s{2,}/g, " ")          // Collapse multiple spaces into a single space
         .replace(/(\s*\n\s*)+/g, "\n")
         .replace()// Remove spaces around newlines
-        .trim();*/
+        .trim();*!/
 
     let textWithoutHtml = content.innerHTML
         .replace(/<[^>]*>/g, "")    // Remove all HTML tags
@@ -50,4 +133,4 @@ const onSubmitHandler2 = event => {
     // let form = document.getElementById("writePostForm");
 
     // form.submit;
-}
+}*/
