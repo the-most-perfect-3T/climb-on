98b79627167b13ea58c530dd00a470c8093f8ec8
cjmin-n <mymemose@naver.com>
2024-11-17 20:05:05 +0900

    feat: mypage 프로필이미지 수정 하는중

--- climb-on/src/main/java/com/ohgiraffers/climbon/user/Enum/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.ohgiraffers.climbon.user.Enum;
-
-public enum UserType {
-    INDOOR,
-    OUTDOOR;
-}
--- climb-on/src/main/java/com/ohgiraffers/climbon/user/controller/UserController.java
+++ climb-on/src/main/java/com/ohgiraffers/climbon/user/controller/UserController.java
@@ -14,52 +14,99 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping("/mypage/*")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("home")
-    public ModelAndView mypage(ModelAndView mv, @AuthenticationPrincipal AuthDetail userDetails) {
-
-        Integer key = userDetails.getLoginUserDTO().getId();
-        UserDTO user = userService.findByKey(key);
-        String crewName = userService.findCrewName(key);
-        String homeName = userService.findHomeName(key);
-
-        if(user == null) {
-            // 찾아온 결과가 없을 때...
-        }
+    private void populateUserData(ModelAndView mv, Integer userId) {
+        UserDTO user = userService.findByKey(userId);
+        String crewName = userService.findCrewName(userId);
+        String homeName = userService.findHomeName(userId);
 
         mv.addObject("user", user);
         mv.addObject("crewName", crewName);
         mv.addObject("homeName", homeName);
+    }
 
+    @GetMapping("home")
+    public ModelAndView mypage(ModelAndView mv, @AuthenticationPrincipal AuthDetail userDetails) {
+        if (userDetails == null || userDetails.getLoginUserDTO() == null) {
+            mv.addObject("message", "로그인 정보가 유효하지 않습니다. 다시 로그인해주세요.");
+            mv.setViewName("/auth/login");
+            return mv;
+        }
+
+        Integer key = userDetails.getLoginUserDTO().getId();
+        UserDTO user = userService.findByKey(key);
+
+        if (user == null) {
+            mv.addObject("message", "사용자 정보를 찾을 수 없습니다.");
+            mv.setViewName("/auth/login");
+            return mv;
+        }
+
+        populateUserData(mv, key);
         mv.setViewName("mypage/mypage");
         return mv;
     }
 
     @PostMapping("updateUser")
-    public ModelAndView updateUser(ModelAndView mv, @AuthenticationPrincipal AuthDetail userDetails, UserDTO user) {
+    public ModelAndView updateUser(
+            ModelAndView mv,
+            @AuthenticationPrincipal AuthDetail userDetails,
+            UserDTO user,
+            RedirectAttributes redirectAttributes) {
+
+        if (userDetails == null || userDetails.getLoginUserDTO() == null) {
+            redirectAttributes.addFlashAttribute("message", "로그인 정보가 유효하지 않습니다.");
+            return new ModelAndView("redirect:/auth/login");
+        }
 
         Integer key = userDetails.getLoginUserDTO().getId();
-        user.setId(key);
-        System.out.println(user.getNickname());
-        System.out.println(user.getPassword());
-        System.out.println(user.getOneLiner());
-        int result = userService.updateUser(user);
-        System.out.println(result);
+        int result = userService.updateUser(user, key);
 
-        if(result > 0){
-
-            mv.addObject("message", "회원정보를 수정했습니다.");
-            mv.setViewName("mypage/mypage");
-        }else {
-
+        if (result > 0) {
+            redirectAttributes.addFlashAttribute("message", "회원정보를 수정했습니다.");
+            return new ModelAndView("redirect:/mypage/home");
+        } else {
+            populateUserData(mv, key);
             mv.addObject("message", "회원정보 수정에 실패했습니다.");
+            mv.setViewName("mypage/mypage");
+            return mv;
+        }
+    }
+
+
+
+    @PostMapping("profileModify")
+    public ModelAndView updateProfile(
+            @AuthenticationPrincipal AuthDetail userDetails,
+            UserDTO user,
+            RedirectAttributes redirectAttributes,
+            ModelAndView mv) {
+
+        if (userDetails == null || userDetails.getLoginUserDTO() == null) {
+            redirectAttributes.addFlashAttribute("message", "로그인이 필요합니다.");
+            return new ModelAndView("redirect:/auth/login");
+        }
+
+
+        Integer key = userDetails.getLoginUserDTO().getId();
+
+        int result = userService.updateProfile(user, key);
+
+
+
+        if (result > 0) {
+            mv.addObject("message", "프로필 이미지를 수정했습니다.");
+            mv.setViewName("redirect:/mypage/home");
+        } else {
+            mv.addObject("message", "프로필 이미지 수정에 실패했습니다.");
             mv.setViewName("mypage/mypage");
         }
 
