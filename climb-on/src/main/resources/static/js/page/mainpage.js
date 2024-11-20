dcd927989ade303a1c68b0709231751a4b9d15c3
yzzzang <yz1211ch@gmail.com>
2024-11-20 12:09:44 +0900

    fix: 500 에러 수정 중 ...

--- climb-on/src/main/resources/static/js/page/mainpage.js
+++ climb-on/src/main/resources/static/js/page/mainpage.js
@@ -1,8 +1,14 @@
-const loadRecentPosts = () =>
+const loadRecentPosts = (category = "") =>
 {
-    fetch(`/api/posts/recent/paginated`)
-        .then(response => response.json())
+    fetch(`/api/posts/recent/paginated?category=${category}`)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error(`${response.status} 에러가 발생했습니다`);
+            }
+            return response.json();
+        })
         .then(posts => {
+            console.log(posts);
             const recentPostsContainer = document.getElementById('recent-posts');
             posts.forEach(post => {
                 const row = document.createElement('tr');
@@ -12,13 +18,19 @@ const loadRecentPosts = () =>
                         `;
                 recentPostsContainer.appendChild(row);
             });
-        });
+        })
+        .catch(error => console.log(error.message));
 }
 
 const loadPopularPosts = () =>
 {
     fetch(`/api/posts/popular`)
-        .then(response => response.json())
+        .then(response => {
+            if(!response.ok) {
+                throw new Error(`${response.status} 에러가 발생했습니다`);
+            }
+            return response.json();
+        })
         .then(posts => {
             const popularPostsContainer = document.getElementById('popular-posts');
             posts.forEach(post => {
@@ -27,7 +39,22 @@ const loadPopularPosts = () =>
                 popularPostsContainer.appendChild(listItem);
             });
         })
+        .catch(error => console.log(error.message));
 }
+
+document.getElementById('category-tabs').addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const selectedCategory = event.target.getAttribute('data-category');
+        currentCategory = selectedCategory; // 현재 카테고리를 선택한 카테고리로 설정
+        loadRecentPosts(currentCategory);
+
+        // Update active tab styling
+        document.querySelectorAll('#category-tabs .nav-link').forEach(tab => {
+            tab.classList.remove('active');
+        });
+        event.target.classList.add('active');
+    }
+});
 
 loadRecentPosts();
 loadPopularPosts();
